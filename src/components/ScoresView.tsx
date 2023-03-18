03f63ee5e0a6c687333febcf39157558a4e88733
kacpercwiklinski <kacper.cwiklinski9@gmail.com>
2023-03-18 14:47:09 +0100

    Fix table buttons color

--- src/components/ScoresView.tsx
+++ src/components/ScoresView.tsx
@@ -105,7 +105,7 @@ const ScoresView = (props: Props) => {
                 <td className="border border-t-0 border-b-0 border-l-0 border-r-1 border-r-accent/40">{userScore.totalWilks.toFixed(2)} pts</td>
                 <td className="border border-t-0 border-b-0 border-l-0">
                   <div className="flex justify-center space-x-2">
-                    <svg onClick={() => handleScoreDelete(userScore.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="m-1 w-6 h-6 text-neutral hover:text-error cursor-pointer transition-colors">
+                    <svg onClick={() => handleScoreDelete(userScore.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="m-1 w-6 h-6 text-accent hover:text-error cursor-pointer transition-colors">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                     </svg>
                   </div>
